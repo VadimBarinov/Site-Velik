e3eceb38425f07dbc44a4d4588f0d518e2a6066c
VadimBarinov <barinovvadim87@gmail.com>
2025-03-10 12:34:45 +0400

    created custom filter get_value_from_dict

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -42,3 +42,8 @@ def show_carousel():
         bikes_characteristics[item.slug] = char_value
 
     return {'bikes': bikes, 'bikes_characteristics': bikes_characteristics}
+
+
+@register.filter
+def get_value_from_dict(dict_data, key):
+    return dict_data.get(key)
