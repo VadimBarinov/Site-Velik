dd4425ef92a9dd8505cd35858f3a12f113fd3601
VadimBarinov <barinovvadim87@gmail.com>
2025-03-18 16:35:25 +0400

    login and logout views

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин',
                                widget=forms.TextInput(attrs={
                                    'class': 'form-input'
@@ -10,3 +12,7 @@ class LoginUserForm(forms.Form):
                                widget=forms.PasswordInput(attrs={
                                    'class': 'form-input'
                                }))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
--- users/views.py
+++ users/views.py
@@ -1,27 +1,35 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
 from django.urls import reverse
 from users.forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password']
-                                )
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {
+        'title': 'Авторизация'
+    }
 
-    else:
-        form = LoginUserForm()
 
-    return render(request, 'users/login.html', {'form': form})
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password']
+#                                 )
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('home'))
+
+#     else:
+#         form = LoginUserForm()
+
+#     return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
