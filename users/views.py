9d76d7f81647cee99875cac56ce43d38d5cdef49
VadimBarinov <barinovvadim87@gmail.com>
2025-03-18 19:22:45 +0400

    fixed bugs

--- users/views.py
+++ users/views.py
@@ -21,15 +21,3 @@ class RegisterUser(CreateView):
         'title': 'Регистрация'
     }
     success_url = reverse_lazy('users:login')
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = RegisterUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data['password'])
-#             user.save()
-#             return render(request, 'users/register_done.html')
-#     else:
-#         form = RegisterUserForm()
-#     return render(request, 'users/register.html', {'form': form})
