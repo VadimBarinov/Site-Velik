87290c8c7bc19bb23d8cef44a6d962241f30898c
VadimBarinov <barinovvadim87@gmail.com>
2025-04-01 14:04:09 +0400

    created js animate function

---
+++ catalog/static/catalog/js/functions.js
@@ -0,0 +1,10 @@
+function clickOnStar(id_star){
+
+    for (let i = 1; i <= id_star; i++){
+        $('#star-modal-' + i).attr('src', grad_star);
+    }
+    for (let i = id_star+1; i <= 5; i++){
+        $('#star-modal-' + i).attr('src', black_star);
+    }
+
+}
