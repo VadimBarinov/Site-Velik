8471c4ab2b7214608f5fd41c3c66d66ceb3d6647
VadimBarinov <barinovvadim87@gmail.com>
2025-04-03 11:12:45 +0400

    added stars page

--- catalog/static/catalog/js/functions.js
+++ catalog/static/catalog/js/functions.js
@@ -10,3 +10,14 @@ function clickOnStar(id_star){
     $('[name=count_stars]').attr('value', id_star)
 
 }
+
+function currentStarsBike(id_star, id_bike){
+
+    for (let i = 1; i <= id_star; i++){
+        $('#star-' + i + '-' + id_bike).attr('src', grad_star);
+    }
+    for (let i = id_star+1; i <= 5; i++){
+        $('#star-' + i + '-' + id_bike).attr('src', black_star);
+    }
+
+}
