b5f92c52e75ecd02fa9c0a684586f47cee99ac5f
VadimBarinov <barinovvadim87@gmail.com>
2025-04-09 22:48:18 +0400

    refactor admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BikeModel, BikeMark, BikeModification, BikeCharacteristic, BikeCharacteristicValue
+from .models import BikeModel, BikeMark, BikeModification, BikeCharacteristic, BikeCharacteristicValue, BikeStars, BikeFavourites
 
 
 @admin.register(BikeModel)
@@ -25,3 +25,13 @@ class BikeCharacteristicAdmin(admin.ModelAdmin):
 @admin.register(BikeCharacteristicValue)
 class BikeCharacteristicValueAdmin(admin.ModelAdmin):
     list_display = ('id', 'value', 'bike_characteristic', 'bike_modification')
+
+
+@admin.register(BikeStars)
+class BikeStarsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bike', 'star')
+
+
+@admin.register(BikeFavourites)
+class BikeFavouritesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bike')
